8c6bc446d4dc9ca28ff76884645143324ef9772d
tnquang1416 <tnquang1416@gmail.com>
2015-03-25 20:49:21 +0700

    reset

--- code/structure/Lattice.java
+++
@@ -1,8 +0,0 @@
-package structure;
-
-import exception.WrongPositionException;
-
-public interface Lattice {
-	public void init() throws WrongPositionException;
-	public void addNode(Node n) throws WrongPositionException;
-}
